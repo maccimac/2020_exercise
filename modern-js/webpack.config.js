723ea1c112e3456c590e64ce621243bfea167634
Margaret Macaranas <margaret.macaranas@gmail.com>
2020-04-30 05:13:44 +0800

    Test moving of dependencies

--- modern-js/webpack.config.js
+++ modern-js/webpack.config.js
@@ -2,5 +2,14 @@ module.exports = {
   entry: "./src/index",
   output: {
     filename: "./dist/app"
+  },
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'baberl-loader'
+      }
+    ]
   }
 }
